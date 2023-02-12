267c7727e8f230f43e528c4de227645689575e62
gildemberg-santos <gildemberg.santos@gmail.com>
2023-02-12 11:56:57 -0300

    Olá, Mundo!

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import ListTask from './ListTask';
-import InputTask from './InputTask';
-import { Save, Load } from './Helper/Actions';
 
 function App() {
-  const [listTask, setListTask] = useState([]);
-
-  const Preferences = {
-    ColorAccent: "#21c25e"
-  }
-
-  const handleNewTask = (currentTask) => {
-    Save(currentTask);
-    setListTask(Load());
-  }
-
-  useEffect(() => {
-    setListTask(Load());
-  }, []);
-
   return (
     <>
-      <h1 style={{marginLeft: 25, marginTop: 25}}>Taks</h1>
-      <InputTask onClick={handleNewTask} accent={Preferences.ColorAccent} placeholder='New Task' />
-      <ListTask list={listTask} style={{ marginTop: 20 }} accent={Preferences.ColorAccent} />
+      <h1>Olá, Rayane!</h1>
+      <p>Vamos começar nossa aula?</p>
     </>
   );
 }
